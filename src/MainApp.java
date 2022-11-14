5ada604fd3e78208119d61981547f6cb2dca5d1c
justmahdii <didoslimani10@gmail.com>
2022-11-14 22:51:25 +0100

    first commit

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
+		//commentaire 
 		EtudiantService serv=new EtudiantService();
 		try {
 			serv.inscription(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx", 1);
