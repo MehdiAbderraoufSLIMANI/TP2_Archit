61dff69a0cfa587d26937a6153a9e6a10b8c3c83
justmahdii <didoslimani10@gmail.com>
2022-11-18 16:29:07 +0100

    question 26

---
+++ src/Package/IDetermineThePackage.java
@@ -0,0 +1,5 @@
+package Package;
+
+public interface IDetermineThePackage {
+	public Package typeOfPackage(TypePackage packag);
+}
---
+++ src/Package/TypePackage.java
@@ -0,0 +1,7 @@
+package Package;
+
+
+public enum TypePackage {
+	Standard,
+	Premium
+}
